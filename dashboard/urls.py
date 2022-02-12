5fa02863c72df043b88ce7f34a1c2e386fec8990
igorface <40202229+igorface@users.noreply.github.com>
2022-02-12 21:17:47 +0100

    created vlans app

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('vlans_list',views.vlans_list, name='vlans_list'),
+    path('admin',views.index,name='admin'),
 ]
---
+++ vlans/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VlansConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vlans'
