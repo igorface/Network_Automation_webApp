14bf9b33ba48394966dc847ef838641ac1b3644d
igorface <40202229+igorface@users.noreply.github.com>
2022-02-27 17:00:42 +0100

    Working adding vlans

--- core/eapi_modules.py
+++ core/eapi_modules.py
@@ -1,9 +1,9 @@
 def create_eapi_conf_file(device, username, password):
-    with open("eapi.conf", "w") as file:
-        file.append(f"""
-        [connection:veos01]
-        host: {device}
-        transport: https
-        username: {username}
-        password: {password}
-        """)
+    with open("core/eapi.conf", "w") as file:
+        device_config_string = (f"""[connection:{device}]
+host: {device}
+transport: https
+username: {username}
+password: {password}
+""")
+        file.write(device_config_string)
--- vlans/views.py
+++ vlans/views.py
@@ -3,7 +3,11 @@ from dashboard.models import Devices
 from django.forms.models import model_to_dict
 from core.get_vlans import create_coroutines_list, gather_vlans
 from core.get_vlans_thread import get_show_vlans_all
+from core.eapi_modules import create_eapi_conf_file
+from core.credentials import get_password, get_username
 from django.contrib import admin,messages
+import pyeapi
+
 
 # Create your views here.
 def list_vlans(request):
@@ -28,19 +32,29 @@ def create_vlan(request):
         'devices': devices,
     }
     if request.method == 'POST':
-        print("HELLO !!!")
         vlan_number_input = request.POST['vlan_number']
         vlan_name = request.POST['vlan_name']
         vlan_number = int(vlan_number_input)
         selected_device = request.POST['device_dropdown'] 
         selected_device_list = request.POST.getlist('device_dropdown')
-        print(selected_device)
-        print(selected_device_list)
+        # print(selected_device)
+        # print(selected_device_list)
         if not vlan_number in range(1,4096):
             messages.error(request, 'Vlan number not in range')
             print('Vlan number not in range')
             return redirect('create_vlan')
         else:
-            print('Vlan number in range')
+            username = get_username()
+            password = get_password()
+            for item in selected_device_list:
+                create_eapi_conf_file(item, username, password)
+                pyeapi.load_config('core/eapi.conf')
+                node = pyeapi.connect_to(f'{item}')
+                create_vlan = node.api('vlans')
+                create_vlan_result = create_vlan.create(vlan_number)
+                create_vlan_name_result = create_vlan.set_name(vlan_number, vlan_name)
+                print(f"Vlan created: {create_vlan_result}" )
+                print(f"Vlan name created: {create_vlan_name_result}" )
+
 
     return render(request, 'vlans/create_vlan.html', context)
